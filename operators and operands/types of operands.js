9de02c0848d1f70afa18bb13b7301a1722fc858f
prasanth <your@email.com>
2025-05-30 13:56:57 +0530

    types of operands

---
+++ operators and operands/types of operands.js
@@ -0,0 +1,24 @@
+/* differnt operators:
+1.unary operator
+2.binary operator
+3.ternary operator */
+
+/* 1.Unary operator: */
+
+let a=10;
+b=-a;
+console.log(b);
+console.log(++a);
+
+/* 2.Binary operator: */
+
+let x=27;
+let y=14;
+let sum=x+y;
+console.log(sum);
+
+/* 3.Ternary operator: */
+
+let prashanth=20;
+let result=prashanth<20 ?"prashanth is less than 20":"prashanth is greater than or equal to 20";
+console.log(result);
