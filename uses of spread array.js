40d23ed5e43e591102204640fa9ae2424bb1cf5e
prasanth <your@email.com>
2025-05-30 22:16:17 +0530

    uses of spread array

---
+++ uses of spread array.js
@@ -0,0 +1,16 @@
+/* spread array */
+const array=[1,2,3,4,5,6,7];
+console.log(...array);
+
+/* Copying array */
+const originalarray=[1,2,3,4];
+const copiedarray=[...originalarray];
+console.log(copiedarray);
+
+/* merging array */
+const array1=[1,2,3,4];
+const array2=[5,6,7,8];
+const mergedarray=[...array1,array2];
+console.log(mergedarray);
+ 
+
